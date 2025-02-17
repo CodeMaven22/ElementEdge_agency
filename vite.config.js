a7e4482b323844503e317c006eea3329a268cf56
CodeMaven22 <tawahcngwira96@gmail.com>
2025-02-17 12:21:44 +0200

    sync

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: process.env.NODE_ENV === 'production' ? 'https://github.com/CodeMaven22/ElementEdge_agency.git/' : '/',
+  base: '/ElementEdge_agency/', // Match your repository name
 })
 // https://github.com/CodeMaven22/ElementEdge_agency.git
